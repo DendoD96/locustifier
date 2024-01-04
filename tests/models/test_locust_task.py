5bbece746d4e37815f0d01d66a1947ef1cdb2d95
Daniele Rossi <daniele.rossi18@studio.unibo.it>
2024-01-04 17:55:12 +0100

    chore: differentiate tests with/without path parameters

--- tests/models/test_locust_task.py
+++ tests/models/test_locust_task.py
@@ -41,10 +41,11 @@ class TestLocustTask(unittest.TestCase):
         self.assertEqual(instance.method, data["method"])
         self.assertEqual(instance.path, data["path"])
 
-    def test_request_code_generation(self):
+    def test_request_code_generation_with_path_parameters(self):
         """
         Test the generation of request code. This test ensures that
         the generated request code matches the expected format.
+        The request path contains path_params.
         """
         expected = """
         def update_user(client):
@@ -78,6 +79,42 @@ class TestLocustTask(unittest.TestCase):
             ),
         )
 
+    def test_request_code_generation_without_path_parameters(self):
+        """
+        Test the generation of request code. This test ensures that
+        the generated request code matches the expected format.
+        """
+        expected = """
+        def update_user(client):
+            client.request(
+                method='POST',
+                url='/user',
+                headers={{'accept': 'application/json'}},
+                params={{'fake_query_param': 'fake'}},
+                json={{'name': '{generated_name}'}},
+            )
+        """
+        data = {
+            "name": "update_user",
+            "method": "POST",
+            "path": "/user",
+            "headers": {"accept": "application/json"},
+            "query_params": {"fake_query_param": "fake"},
+            "req_body": [{"name": "name", "type": "str"}],
+        }
+        instance = LocustTask(**data)
+        generated_request_code = instance.generate_request_code()
+        self.assertEqual(
+            generated_request_code,
+            textwrap.dedent(
+                expected.format(
+                    generated_name=re.search(
+                        r"json={'name': '([a-zA-Z]+)'}", generated_request_code
+                    ).group(1)
+                )
+            ),
+        )
+
     def test_locust_task_code_generation(self):
         expected = """
             @task(1)
@@ -86,12 +123,9 @@ class TestLocustTask(unittest.TestCase):
         """
         data = {
             "name": "update_user",
-            "method": "PUT",
+            "method": "GET",
             "path": "/user/{id}",
             "path_params": [{"name": "id", "value": "123"}],
-            "headers": {"accept": "application/json"},
-            "query_params": {"fake_query_param": "fake"},
-            "req_body": [{"name": "name", "type": "str"}],
         }
         instance = LocustTask(**data)
         generated_locust_task_code = instance.generate_locust_task_code()
