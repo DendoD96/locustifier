2e5b1a50f86aa6d4a9a9ce51aff028124f62ef43
Daniele Rossi <daniele.rossi18@studio.unibo.it>
2024-01-04 17:50:34 +0100

    feat: add request and task code generation

--- sample/models/locust_task.py
+++ sample/models/locust_task.py
@@ -1,9 +1,27 @@
 from http import HTTPMethod
+import textwrap
 from typing import List, Optional
 from pydantic import BaseModel, validator
 from sample.models.fake_body_parameter import FakeBodyParameter
 
 from sample.models.path_parameter import PathParameter
+
+REQUEST_CODE_TEMPLATE = """
+def {function_name}(client):
+    client.request(
+        method='{method}',
+        url='{url}',
+        {optional_headers},
+        {optional_query_params},
+        {optional_req_body},
+    )
+"""
+
+LOCUST_TASK_CODE_TEMPLATE = """
+@task({weight})
+def {task_name}(self):
+    requests.{task_name}(self)
+"""
 
 
 class LocustTask(BaseModel):
@@ -29,3 +47,46 @@ class LocustTask(BaseModel):
             )
 
         return value
+
+    def __get_request_path(self) -> str:
+        if self.path_params:
+            return self.path.format(
+                **{param.name: param.value for param in self.path_params}
+            )
+        return self.path
+
+    def __get_optional_parameter(self) -> dict:
+        optional_parameters = {}
+
+        if self.headers:
+            optional_parameters["optional_headers"] = f"headers={self.headers}"
+        if self.query_params:
+            optional_parameters[
+                "optional_query_params"
+            ] = f"params={self.query_params}"
+        if self.req_body:
+            body_parameter_dict = {
+                param.name: param.generate_value() for param in self.req_body
+            }
+            optional_parameters[
+                "optional_req_body"
+            ] = f"json={body_parameter_dict}"
+
+        return optional_parameters
+
+    def generate_request_code(self):
+        return textwrap.dedent(
+            REQUEST_CODE_TEMPLATE.format(
+                function_name=self.name,
+                method=self.method,
+                url=self.__get_request_path(),
+                **self.__get_optional_parameter(),
+            )
+        )
+
+    def generate_locust_task_code(self):
+        return textwrap.dedent(
+            LOCUST_TASK_CODE_TEMPLATE.format(
+                weight=self.weight, task_name=self.name
+            )
+        )
--- tests/models/test_locust_task.py
+++ tests/models/test_locust_task.py
@@ -1,3 +1,5 @@
+import re
+import textwrap
 import unittest
 from pydantic import ValidationError
 
@@ -38,3 +40,62 @@ class TestLocustTask(unittest.TestCase):
         self.assertEqual(instance.name, data["name"])
         self.assertEqual(instance.method, data["method"])
         self.assertEqual(instance.path, data["path"])
+
+    def test_request_code_generation(self):
+        """
+        Test the generation of request code. This test ensures that
+        the generated request code matches the expected format.
+        """
+        expected = """
+        def update_user(client):
+            client.request(
+                method='PUT',
+                url='/user/123',
+                headers={{'accept': 'application/json'}},
+                params={{'fake_query_param': 'fake'}},
+                json={{'name': '{generated_name}'}},
+            )
+        """
+        data = {
+            "name": "update_user",
+            "method": "PUT",
+            "path": "/user/{id}",
+            "path_params": [{"name": "id", "value": "123"}],
+            "headers": {"accept": "application/json"},
+            "query_params": {"fake_query_param": "fake"},
+            "req_body": [{"name": "name", "type": "str"}],
+        }
+        instance = LocustTask(**data)
+        generated_request_code = instance.generate_request_code()
+        self.assertEqual(
+            generated_request_code,
+            textwrap.dedent(
+                expected.format(
+                    generated_name=re.search(
+                        r"json={'name': '([a-zA-Z]+)'}", generated_request_code
+                    ).group(1)
+                )
+            ),
+        )
+
+    def test_locust_task_code_generation(self):
+        expected = """
+            @task(1)
+            def update_user(self):
+                requests.update_user(self)
+        """
+        data = {
+            "name": "update_user",
+            "method": "PUT",
+            "path": "/user/{id}",
+            "path_params": [{"name": "id", "value": "123"}],
+            "headers": {"accept": "application/json"},
+            "query_params": {"fake_query_param": "fake"},
+            "req_body": [{"name": "name", "type": "str"}],
+        }
+        instance = LocustTask(**data)
+        generated_locust_task_code = instance.generate_locust_task_code()
+        self.assertEqual(
+            generated_locust_task_code,
+            textwrap.dedent(expected),
+        )
